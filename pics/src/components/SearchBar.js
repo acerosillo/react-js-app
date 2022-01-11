1d13bd1d521b9eb25e00d1d5a3ac1cde1bd4a6ef
HALVARSSON\ace.rosillo <ace.rosillo@comprend.com>
2022-01-11 12:03:05 +0000

    change

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,21 +5,13 @@ class SearchBar extends React.Component {
     console.log(e.target.value);
   }
 
-  onInputClick() {
-    console.log("input was clicked");
-  }
-
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input
-              type="text"
-              onClick={this.onInputClick}
-              onChange={this.onInputChange}
-            ></input>
+            <input type="text" onChange={this.onInputChange}></input>
           </div>
         </form>
       </div>
