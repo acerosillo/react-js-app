d045245b6b5946002e05ae7b497fedf9a4f7d033
acerosillo <ace.rosillo@comprend.com>
2022-07-13 16:30:54 +0100

    changes

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 //import unsplash from "../api/unsplash";
-//import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar";
 //import ImageList from "./ImageList";
 
 const App = () => {
-  return <div>hello ace</div>;
+  return (
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,24 +1,17 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "" };
-
-  onFormSubmit = (e) => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.term);
-  };
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            ></input>
+            <input type="text" onChange={this.onInputChange}></input>
           </div>
         </form>
       </div>
